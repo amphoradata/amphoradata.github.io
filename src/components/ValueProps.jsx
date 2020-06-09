a1d2ef14a32ce7581ec4dba20466d04ba859d759
Rian Finnegan <rian@amphoradata.com>
2020-06-09 13:41:55 +1000

    feat: value props component

--- src/components/ValueProps.jsx
+++ src/components/ValueProps.jsx
@@ -4,107 +4,86 @@ import Link from "@docusaurus/Link";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCheckCircle } from "@fortawesome/free-regular-svg-icons";
 import ReactPlayer from "react-player";
+import { PrimaryButton } from "./PrimaryButton";
+
+import "./valueProps.css";
+
+const PropRow = (props) => (
+    <div className="d-flex prop">
+        <FontAwesomeIcon
+            icon={faCheckCircle}
+            size="2x"
+            className="text--success"
+        />
+        &nbsp;
+        {props.children}
+    </div>
+);
 
 export const ValueProps = (props) => {
     return (
-        <div className={classnames("p-4", props.styles.presentationContainer)}>
+        <div className={classnames("p-4 valueProps", props.styles.presentationContainer)}>
             <div className="container container--fluid">
-                <div
-                    className={classnames("row", props.styles.spaceBetween)}
-                    style={{
-                        paddingTop: "120px",
-                        paddingBottom: "60px",
-                        paddingLeft: "240px",
-                        paddingRight: "240px",
-                    }}
-                >
-                    <div>
-                        <h2 className="font_extra_large text--left text--black">
-                            With Amphora you can
-                        </h2>
-                        <div className="d-flex">
-                            <FontAwesomeIcon
-                                icon={faCheckCircle}
-                                size="lg"
-                                className="text--primary  text--black"
-                            />
-                            &nbsp;
+                <div className={classnames("row", props.styles.spaceBetween)}>
+                    <div className="col col--6">
+                        <h3 className="heading font_extra_large text--black">
+                            With Amphora you can...
+                        </h3>
+                        <PropRow>
                             <Link
                                 className={classnames(props.styles.getStarted)}
                                 to="how-we-help/find-data"
                             >
                                 <span className="font_extra_large text--black">
-                                    Access third-party data&nbsp;&nbsp;
+                                    <strong>Access</strong> third-party data
                                 </span>
                             </Link>
-                        </div>
-                        <div className="d-flex">
-                            <FontAwesomeIcon
-                                icon={faCheckCircle}
-                                size="lg"
-                                className="text--primary  text--black"
-                            />
-                            &nbsp;
+                        </PropRow>
+                        <PropRow>
                             <Link
                                 className={classnames(props.styles.getStarted)}
                                 to="how-we-help/simplified-integration"
                             >
                                 <span className="font_extra_large text--black">
-                                    Simplify external integration&nbsp;&nbsp;
+                                    <strong>Simplify</strong> external integration
                                 </span>
                             </Link>
-                        </div>
-                        <div className="d-flex">
-                            <FontAwesomeIcon
-                                icon={faCheckCircle}
-                                size="lg"
-                                className="text--primary  text--black"
-                            />
-                            &nbsp;
+                        </PropRow>
+                        <PropRow>
                             <Link
                                 className={classnames(props.styles.getStarted)}
                                 to="how-we-help/share-data"
                             >
                                 <span className="font_extra_large  text--black">
-                                    Share your data&nbsp;&nbsp;
+                                    <strong>Share</strong> your data
                                 </span>
                             </Link>
-                        </div>
-                        <div className="d-flex">
-                            <FontAwesomeIcon
-                                icon={faCheckCircle}
-                                size="lg"
-                                className="text--primary  text--black"
-                            />
-                            &nbsp;
+                        </PropRow>
+                        <PropRow>
                             <Link
                                 className={classnames(props.styles.getStarted)}
                                 to="how-we-help/manage-data"
                             >
-                                <span className="font_extra_large  text--black">
-                                    Manage data centrally&nbsp;&nbsp;
+                                <span className="font_extra_large text--black">
+                                    <strong>Manage</strong> data centrally
                                 </span>
                             </Link>
-                        </div>
+                        </PropRow>
                     </div>
-                    <div>
-                        <div style={{ width: "650px" }}>
-                            <ReactPlayer
-                                url="https://youtu.be/rBt_743oT18"
-                                width="100%"
-                            />
-                        </div>
+                    <div className="col col--6 col-video">
+                        <ReactPlayer
+                            url="https://youtu.be/rBt_743oT18"
+                            width="100%"
+                        />
                     </div>
                 </div>
             </div>
-            <div className={props.styles.buttons}>
-                <Link
-                    className="font_medium text--white"
-                    to={"https://identity.amphoradata.com/Account/Register"}
-                >
-                    Register for Free
-                </Link>
-            </div>
+            <Link
+                className="font_medium text--white"
+                to={"https://identity.amphoradata.com/Account/Register"}
+            >
+                <PrimaryButton>Register for Free</PrimaryButton>
+            </Link>
         </div>
     );
 };
