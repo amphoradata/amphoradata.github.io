06dffea8f9b7dadacda902adc89e23b2718f6869
Rian Finnegan <rian@amphoradata.com>
2020-06-09 14:12:19 +1000

    remove unused

--- src/pages/index.js
+++ src/pages/index.js
@@ -11,21 +11,6 @@ import {Landing} from "../components/Landing";
 import { ValueProps } from "../components/ValueProps";
 import { MoreInfo } from "../components/MoreInfoComponent";
 
-function Feature({ imageUrl, title, description }) {
-  const imgUrl = useBaseUrl(imageUrl);
-  return (
-    <div className={classnames("col col--4", styles.feature)}>
-      {imgUrl && (
-        <div className="text--center">
-          <img className={styles.featureImage} src={imgUrl} alt={title} />
-        </div>
-      )}
-      <h3>{title}</h3>
-      <p>{description}</p>
-    </div>
-  );
-}
-
 function Home() {
   const context = useDocusaurusContext();
   const { siteConfig = {} } = context;
@@ -42,19 +27,6 @@ function Home() {
           <ValueProps styles={styles} />
           <MoreInfo styles={styles} />
         </main>
-        {/* <main>
-        {features && features.length && (
-          <section className={styles.features}>
-            <div className="container">
-              <div className="row">
-                {features.map((props, idx) => (
-                  <Feature key={idx} {...props} />
-                ))}
-              </div>
-            </div>
-          </section>
-        )}
-      </main> */}
       </Layout>
     </React.Fragment>
   );
