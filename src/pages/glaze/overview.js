90b4f3ded26cae63e5462892e99a3e557a129491
Rian Finnegan <rian@amphoradata.com>
2020-07-30 13:02:46 +1000

    copy: shorter wording

--- src/pages/glaze/overview.js
+++ src/pages/glaze/overview.js
@@ -4,39 +4,39 @@ import Layout from "@theme/Layout";
 import styles from ".././styles.module.css";
 import { GlazeCard } from "../../components/GlazeOverview";
 
-const pricing = (props) => {
-    const info = {
-        id: 0,
-        title: "Glaze",
-        imageUrl: "/img/Amphora_glaze_title_two_lines.png",
-        perUsers: "Coming soon",
-        fee:
-            "The single-click solution for teams and companies to share and sell data",
-        isMostPopular: false,
-        actionName: "Learn more",
-        actionLink: "mailto:contact@amphoradata.com",
-        descriptions: [
-            {
-                text:
-                    "Instantly deployable platform-as-a-service to sell data through your own website",
-            },
-            {
-                text:
-                    "Get a new revenue channel by selling your own insights on Amphora Marketplace",
-            },
-            {
-                text:
-                    "Utilise our inbuilt customer identity management which supports user specific data access and management",
-            },
-            {
-                text: "Seamless payments handling so you get paid more faster",
-            },
-            {
-                text:
-                    "Leading data management and usage tools to give you full transparency and help you adhere to GDPR",
-            },
-        ],
-    };
+const info = {
+    id: 0,
+    title: "Glaze",
+    imageUrl: "/img/Amphora_glaze_title_two_lines.png",
+    perUsers: "Coming soon",
+    fee: "The single-click solution to share and sell data",
+    isMostPopular: false,
+    actionName: "Learn more",
+    actionLink: "mailto:contact@amphoradata.com",
+    descriptions: [
+        {
+            text:
+                "Instantly deployable platform to sell data through your own website",
+        },
+        {
+            text:
+                "Get a new revenue channel by selling your own insights on Amphora Marketplace",
+        },
+        {
+            text:
+                "Utilise our inbuilt customer identity management which supports user specific data access and management",
+        },
+        {
+            text: "Seamless payments handling so you get paid more faster",
+        },
+        {
+            text:
+                "Leading data management and usage tools to give you full transparency and help you adhere to GDPR",
+        },
+    ],
+};
+
+const page = () => {
     return (
         <Layout title="Pricing">
             <div
@@ -62,4 +62,4 @@ const pricing = (props) => {
     );
 };
 
-export default pricing;
+export default page;
