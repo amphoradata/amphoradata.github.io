16afaef1e979591ebc24408b664b1bfe7c7f36e6
Rian Finnegan <rian@amphoradata.com>
2020-10-28 12:22:34 +1100

    covert index to tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,11 +6,7 @@ import styles from "./styles.module.css";
 import { CustomHead } from "../components/CustomHead";
 import { Landing } from "../components/Landing";
 import { Logos } from "../components/Logos";
-import { ValueProps } from "../components/ValueProps";
 import { BuyData } from "../components/BuyData";
-import { SellData } from "../components/SellData";
-import { MailingListSubscribeSection } from "../components/MailingListSubscribe";
-import { HowWeThinkAboutData } from "../components/HowWeThinkAboutData";
 
 function Home() {
     const context = useDocusaurusContext();
